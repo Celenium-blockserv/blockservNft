b431a506cea5010091013534f0935e53eabfb4dc
Guillaume Jourdain <guillaume.jourdain@orange.com>
2022-09-07 17:17:44 +0200

    [WIP] truffle migrate --network rinkeby

--- client/src/components/Nft/index.jsx
+++ client/src/components/Nft/index.jsx
@@ -1,76 +1,91 @@
 import { useState } from "react";
 import useEth from "../../contexts/EthContext/useEth";
 import Title from "./Title";
-import Cta from "./Cta";
-import Contract from "./Contract";
-import ContractBtns from "./ContractBtns";
-import Desc from "./Desc";
 import NoticeNoArtifact from "./NoticeNoArtifact";
 import NoticeWrongNetwork from "./NoticeWrongNetwork";
 
-import * as Constants from "../../config"
-//import { create } from "ipfs-http-client";
-
-//const client = create('https://ipfs.infura.io:5001/api/v0');
 
 
+const { NFTStorage } = require("nft.storage/dist/bundle.esm.min.js");
 
 function Nft() {
   const { state } = useEth();
   const [value, setValue] = useState("?");
   const [file, setFile] = useState(null);
+  const [data, setData] = useState(null);
+  const [urlArr, setUrlArr] = useState([]);
+
+  let NFTStorageClient = {};
 
 
+  const [apiKeyValue, setApiKeyValue] = useState("");
 
+  const handleInputChange = e => {
 
-
+    setApiKeyValue(e.target.value);
+    NFTStorageClient = new NFTStorage({ token:apiKeyValue });
+  };
 
   const retrieveFile = (e) => {
-    const data = e.target.files[0];
-    const reader = new window.FileReader();
-    reader.readAsArrayBuffer(data);
-    reader.onloadend = () => {
-      console.log("Buffer data: ", Buffer(reader.result));
-      setFile(Buffer(reader.result));
-    }
+    console.log('retrieveFile');
 
-    e.preventDefault();
+    const data = e.target.files[0];
+
+    setData(data);
+    console.log(data);
+
+
+    // const reader = new window.FileReader();
+    // reader.readAsArrayBuffer(data);
+    // reader.onloadend = () => {
+    //   console.log("Buffer data: ", Buffer(reader.result));
+    //   setFile(Buffer(reader.result));
+    // }
+    //
+    // e.preventDefault();
   }
 
 
 
-  const [urlArr, setUrlArr] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      //const created = await client.add(file);
-      //const url = `https://ipfs.infura.io/ipfs/${created.path}`;
-      const url = `https://ipfs.infura.io/ipfs/`;
-      setUrlArr(prev => [...prev, url]);
-      console.log('handleSubmit')
-      console.log(Constants.NFS_STORAGE_API_KEY)
-    } catch (error) {
-      console.log(error.message);
-    }
+    console.log('Handle submit');
+    console.log(data)
+    const image = data;
+
+    const cid = await NFTStorageClient.storeDirectory([new File([image], "NFT")]);
+
+    console.log(cid);
+
+    // e.preventDefault();
+    // try {
+    //   //const created = await client.add(file);
+    //   //const url = `https://ipfs.infura.io/ipfs/${created.path}`;
+    //   const url = `https://ipfs.infura.io/ipfs/`;
+    //   setUrlArr(prev => [...prev, url]);
+    //   console.log('handleSubmit')
+    // } catch (error) {
+    //   console.log(error.message);
+    // }
   };
 
 
 
   const demo =
     <>
-      <Cta />
-      <div className="contract-container">
-        <Contract value={value} />
-        <ContractBtns setValue={setValue} />
-      </div>
-      <Desc />
       <div>
         <form className="form" onSubmit={handleSubmit}>
           <input type="file" name="data" onChange={retrieveFile} />
           <button type="submit" className="btn">Upload file</button>
         </form>
       </div>
+      <input
+        type="text"
+        placeholder="nft storage api key"
+        value={apiKeyValue}
+        onChange={handleInputChange}
+      />
     </>;
 
   return (
